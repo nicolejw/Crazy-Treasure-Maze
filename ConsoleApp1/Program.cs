faff6f9f33ffb9ccbe38c7f1e4507ff22315cabf
Nicole Wilkie <nicolejwilkie@gmail.com>
2018-06-04 04:35:51 +1200

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,20 +12,16 @@ namespace ConsoleApp1
         {
             //-----This section of code is intended to handle the seed, checks if an argument has been input when running the application.
             //if it has, the game will use the input as the rng seed. If not, the starting positions of the game entities will be set to a default value-------
-            int charX= 3;
-            int charY=3;
             Random random = new Random();
-
             Console.Title = "Crazy Treasure Maze";
-            int mapX = 10;
-            int mapY = 10;
             bool endGame = false;
 
             while (endGame == false)
             {
-                Console.SetCursorPosition(charX, charY);
-                Console.Write("S");
-
+                int charX;
+                int charY;
+                int mapX = 10;
+                int mapY = 10;
                 char[,] grid = new char[mapX, mapY];
                 for (int x = 0; x < mapX; ++x)
                 {
@@ -34,23 +30,27 @@ namespace ConsoleApp1
                         grid[x, y] = ' ';
                     }
                 }
-
-                //if (args == null || args.Length == 0)
-                //{
-                //    charX = 5;
-                //    charY = 5;
-                //}
-                //else
-                //{
-                //    //---this line is just for checking whether the input is actually grabbed or not, and should just print the value----
-                //    //Console.WriteLine(Convert.ToInt32(args));
-                //    charX = 2;
-                //    charY = 2;
-                //}
-
-
                 Console.Clear();
                 ShowGrid(grid);
+
+
+                if (args == null || args.Length == 0)
+                {
+                    charX = 5;
+                    charY = 5;
+                }
+                else
+                {
+                    //---this line is just for checking whether the input is actually grabbed or not, and should just print the value----
+                    //Console.WriteLine(Convert.ToInt32(args));
+                    charX = 2;
+                    charY = 2;
+                }
+
+                Console.SetCursorPosition(charX, charY);
+                Console.Write("S");
+                //Console.ReadKey();
+
 
                 ConsoleKeyInfo playerInput = Console.ReadKey(true);
                 switch (playerInput.Key)
