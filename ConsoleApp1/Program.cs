ee0d0f4c270c04802db26897a3c9df3df052a997
Nicole Wilkie <nicolejwilkie@gmail.com>
2018-06-04 17:47:27 +1200

    Partially complete, emergency upload since gpu is dying

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,27 +10,46 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //-----This section of code is intended to handle the seed, checks if an argument has been input when running the application.
-            //if it has, the game will use the input as the rng seed. If not, the starting positions of the game entities will be set to a default value-------
-            Random random = new Random();
+            //int seed = 1;
+
             Console.Title = "Crazy Treasure Maze";
             bool endGame = false;
+            bool move = false;
             int charX;
             int charY;
+            int ghostX;
+            int ghostY;
             int mapX = 10;
             int mapY = 10;
-
-            if (args == null || args.Length == 0)
+            int seed;
+            //string argsHolder = args[0];
+            if(args == null)
             {
-                charX = 4;
-                charY = 4;
+                seed = Int32.Parse(args[0]);
+            }
+            else
+            {
+                seed = 0;
+            }
+            Random random = new Random(seed);
+
+            //-----This section of code is intended to handle the seed, checks if an argument has been input when running the application.
+            //if it has, the game will use the input as the rng seed. If not, the starting positions of the game entities will be set to a default value-------
+            if (seed == 0)
+            {
+                charX = 1;
+                charY = 1;
+                ghostX = 5;
+                ghostY = 5;
             }
             else
             {
                 //---this line is just for checking whether the input is actually grabbed or not, and should just print the value----
-                //Console.WriteLine(Convert.ToInt32(args));
-                charX = 2;
-                charY = 2;
+                //Console.WriteLine(args);
+                charX = random.Next(1, 10);
+                charY = random.Next(1, 10);
+                ghostX =random.Next(1, 10);
+                ghostY =random.Next(1, 10);
             }
 
             while (endGame == false)
@@ -49,6 +68,8 @@ namespace ConsoleApp1
                 Console.SetCursorPosition(charX, charY);
                 Console.Write("S");
                 //Console.ReadKey();
+                Console.SetCursorPosition(ghostX, ghostY);
+                Console.Write("G");
 
 
                 ConsoleKeyInfo playerInput = Console.ReadKey(true);
@@ -71,7 +92,52 @@ namespace ConsoleApp1
                             charX++;
                         break;
                 }
-
+                ConsoleKeyInfo ghostMove = Console.ReadKey(true);
+                int newPos = random.Next(4);
+                switch (ghostMove.Key)
+                {
+                    case ConsoleKey.N:
+                        if (ghostY > 0)
+                            if(newPos == 1)
+                            {
+                                ghostY--;
+                            }
+                        if(ghostY > 0)
+                        {
+                            if(newPos == 2)
+                            {
+                                ghostY++;
+                            }
+                        }
+                        if (ghostY > 0)
+                        {
+                            if (newPos == 3)
+                            {
+                                ghostX--;
+                            }
+                        }
+                        if (ghostY > 0)
+                        {
+                            if (newPos == 4)
+                            {
+                                ghostX++;
+                            }
+                        }
+                        break; //-----The above is to test randomization of the ghosts movement. Probably contains needless code/theres an easier way of 
+                        //doing this? -----
+                    case ConsoleKey.S:
+                        if (ghostY < 9)
+                            ghostY++;
+                        break;
+                    case ConsoleKey.W:
+                        if (ghostX > 0)
+                            ghostX--;
+                        break;
+                    case ConsoleKey.E:
+                        if (ghostX < 9)
+                            ghostX++;
+                        break;
+                }
                 //-------The following code allows player movement through the specified keys, but will not check coordinates against
                 //the bounds of our array, resulting in out of bounds moving to the edge of the console (top and left), and allowing any movement within the whole
                 //console widow (right and down)--------------------
@@ -107,15 +173,22 @@ namespace ConsoleApp1
                 }
                 Console.WriteLine();
             }
-        }       
+        }  
 
-        //static void movement()
+        static void ghostMove()
+        {
+
+        }
+        //static void rngGen(Random ra)
         //{
-        //    bool active = true;
-        //    while (active == true)
-        //    {
-        //        var next = 
-        //    }
+        //    for (int)
+        //}
+
+        //static void seedGen(int seed)
+        //{
+        //    Random random = new Random(seed);
+
+
         //}
     }
 }
